732f07fe1dcb86620b93ea9aa96cdd0a8c7ce709
lucas-faget <lucasfaget98@gmail.com>
2025-09-15 21:03:28 +0200

    build: setup i18n with paraglide

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
     import favicon from "$lib/assets/favicon.svg";
     import "../app.css";
+    import { page } from "$app/state";
+    import { locales, localizeHref } from "$lib/i18n/runtime";
+
+    export const prerender = true;
 
     let { children } = $props();
 </script>
@@ -10,3 +14,9 @@
 </svelte:head>
 
 {@render children?.()}
+
+<div style="display:none">
+    {#each locales as locale}
+        <a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
+    {/each}
+</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [sveltekit(), tailwindcss()],
+    plugins: [
+        sveltekit(),
+        tailwindcss(),
+        paraglideVitePlugin({
+            project: "./project.inlang",
+            outdir: "./src/lib/i18n",
+            strategy: ["url", "cookie", "baseLocale"],
+        }),
+    ],
 });
