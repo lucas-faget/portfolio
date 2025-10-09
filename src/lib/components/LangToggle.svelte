3243176d82f78cbd3a575a7b9e0d7857a11ed792
lucas-faget <lucasfaget98@gmail.com>
2025-10-09 21:09:00 +0200

    feat(components): add LangToggle

---
+++ src/lib/components/LangToggle.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { getLocale, setLocale } from "$lib/i18n/runtime";
+
+    let isFr: boolean = true;
+
+    onMount(() => {
+        const lang = getLocale();
+        isFr = lang === "fr";
+
+        applyLang();
+    });
+
+    function toggleLang(): void {
+        isFr = !isFr;
+        applyLang();
+    }
+
+    function applyLang(): void {
+        if (isFr) {
+            setLocale("fr");
+        } else {
+            setLocale("en");
+        }
+    }
+</script>
+
+<button
+    onclick={toggleLang}
+    class="border-input hover:bg-muted inline-flex size-10 shrink-0 items-center justify-center rounded-lg bg-inherit"
+>
+    {#if isFr}
+        <iconify-icon icon="circle-flags:fr" class="text-2xl"></iconify-icon>
+    {:else}
+        <iconify-icon icon="circle-flags:en" class="text-2xl"></iconify-icon>
+    {/if}
+</button>
